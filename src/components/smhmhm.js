91ebb4746d2894022789aa7e3dd187fa54c03fba
Rohit <rohit200827@gmail.com>
2022-03-18 08:36:25 +0530

    first-yo

---
+++ src/components/smhmhm.js
@@ -0,0 +1,13 @@
+const content = ()=>{
+    return (
+        <div className="content">
+            {/* <h1>Hello World</h1> */}
+            <button className="btn-secondary btn-hide">Option 1</button>
+            <button className="btn-secondary btn-hide">Option 2 </button>
+            <button className="btn-secondary btn-hide">Option 3 </button>
+            <button className="btn-secondary btn-hide">Option 4 </button>
+        </div>
+    )
+}
+
+export default content;
